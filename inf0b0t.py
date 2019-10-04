0ecc70efba4ccd8897e84ac9f02a03f16748af56
sm38 <56010728+sm38@users.noreply.github.com>
2019-10-04 09:30:56 +0800

    Update inf0b0t.py

--- inf0b0t.py
+++ inf0b0t.py
@@ -6,9 +6,9 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     if message.text == 'ты попугай ?':
-       ans_txt = 'Да'
+       ans_text = 'Да'
     else:
-       ans_txt = 'You typed: '+message.text    
+       ans_text = 'You typed: '+message.text    
     
     bot.send_message(message.chat.id, ans_text)
 
