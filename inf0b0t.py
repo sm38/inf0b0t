d0ffc9d7fc90ee4bab6170e5601397a491296c6e
sm38 <56010728+sm38@users.noreply.github.com>
2019-10-04 10:51:08 +0800

    cerry added

--- inf0b0t.py
+++ inf0b0t.py
@@ -1,11 +1,11 @@
-import config
+﻿import config
 import telebot
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    if message.text == 'ты попугай ?':
+    if message.text == 'Ты попугай ?':
        ans_text = 'Да'
     else:
        ans_text = 'You typed: '+message.text    
